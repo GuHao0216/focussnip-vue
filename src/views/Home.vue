2a35b910ac7a9dae5457e7c6e256773d9f7bfe0e
Sumixer@GH <1272639290@qq.com>
2022-03-22 18:54:07 +0800

    引入element-ui-plus

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="common-layout" style="height:auto">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+          <HomeMain />
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeMain from '@/components/main/HomeMain.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeMain
+  }
+}
+</script>
